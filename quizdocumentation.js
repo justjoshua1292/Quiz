7674138f062997c74ade4e5bf149998871366923
Joshua Harris <Justjoshua1292@gmail.com>
2021-08-04 14:15:27 -0400

    editing my list of questions, going to see if this works

---
+++ quizdocumentation.js
@@ -0,0 +1,28 @@
+var MyListOfQuestions = [
+    {
+        title: "Which property is used to change the background color? : ",
+        choices: ["bgcolor", "color", "background-color"],
+        answer: "background-color"
+    },
+    {
+        title: "Which property is used to change the background color? : ",
+        choices: ["bgcolor", "color", "background-color"],
+        answer: "background-color"
+    },
+    {
+        title: "Which property is used to change the background color? : ",
+        choices: ["bgcolor", "color", "background-color"],
+        answer: "background-color"
+    },
+    {
+        title: "Which property is used to change the background color? : ",
+        choices: ["bgcolor", "color", "background-color"],
+        answer: "background-color"
+    },
+    {
+        title: "Which property is used to change the background color? : ",
+        choices: ["bgcolor", "color", "background-color"],
+        answer: "background-color"
+    }
+
+];
