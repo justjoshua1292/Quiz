154d532911b7f4cdd81b34d73ebddd543bae71f6
Joshua Harris <Justjoshua1292@gmail.com>
2021-08-04 12:35:10 -0400

    adding elements to ID's and my variables for my quiz

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-var 
-var
-var
-var
-var
+var myQuestions = 0;
+var timing = questions.length *30;
+var timerLock;
+var sfxCorrect = new Audio ("");
+var sfxIncorrect = new Audio ("");
+
+var questionsListEl= document.getElementById("my-questions");
+var mytimerEl= document.getElementById("time");
+var myChoicesEl = document.getElementById("choices");
+var MySubmitBtn = document.getElementById("submit");
+var MyInitalsEl = document.getElementById("names-initals");
+var startBtn = document.getElementById("start");
+var MyfeedbackEl = document.getElementById("feedback");
